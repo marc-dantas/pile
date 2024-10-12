5df09971723f717b1e7b787f7b80ba0022aef4d1
marc-dantas <marciodantasms@hotmail.com>
2024-10-12 19:26:15 -0300

    refactor ops to handle string and number types uniformly in runtime

--- src/runtime.rs
+++ src/runtime.rs
@@ -19,13 +19,6 @@ pub struct Namespace<'a> {
     pub defs: Vec<Definition>,
 }
 
-pub enum NumberBinaryOp {
-    Add, Sub, Mul, Div,
-    Gt, Lt, Eq, Ge, Le, Ne,
-    Shl, Shr, Bor, Band,
-    Swap, Over
-}
-
 // stack operations:
 // dup (a -- a a)
 // drop (a -- )
@@ -33,38 +26,44 @@ pub enum NumberBinaryOp {
 // over (a b -- a b a)
 // rot (a b c -- b c a)
 
-pub enum NumberUnaryOp {
+pub enum BinaryOp {
+    Add, Sub, Mul, Div,
+    Gt, Lt, Eq, Ge, Le, Ne,
+    Shl, Shr, Bor, Band,
+    Swap, Over
+}
+
+pub fn binaryop_readable(x: BinaryOp) -> String {
+    match x {
+        BinaryOp::Add => String::from("+"),
+        BinaryOp::Sub => String::from("-"),
+        BinaryOp::Mul => String::from("*"),
+        BinaryOp::Div => String::from("/"),
+        BinaryOp::Gt => String::from(">"),
+        BinaryOp::Lt => String::from("<"),
+        BinaryOp::Eq => String::from("="),
+        BinaryOp::Ge => String::from(">="),
+        BinaryOp::Le => String::from("<="),
+        BinaryOp::Ne => String::from("!="),
+        BinaryOp::Shl => String::from(">>"),
+        BinaryOp::Shr => String::from("<<"),
+        BinaryOp::Bor => String::from("|"),
+        BinaryOp::Band => String::from("&"),
+        BinaryOp::Swap => String::from("swap"),
+        BinaryOp::Over => String::from("over"),
+    }
+}
+
+pub enum UnaryOp {
     Print, Dup, Drop
 }
 
-pub fn numberunaryop_readable(x: NumberUnaryOp) -> String {
-    return match x {
-        NumberUnaryOp::Print => String::from("print"),
-        NumberUnaryOp::Dup => String::from("dup"),
-        NumberUnaryOp::Drop => String::from("drop"),
-    };
-}
-
-// maybe implement some Format thing idk
-pub fn numberbinaryop_readable(x: NumberBinaryOp) -> String {
-    return match x {
-        NumberBinaryOp::Add => String::from("+"),
-        NumberBinaryOp::Sub => String::from("-"),
-        NumberBinaryOp::Mul => String::from("*"),
-        NumberBinaryOp::Div => String::from("/"),
-        NumberBinaryOp::Gt => String::from(">"),
-        NumberBinaryOp::Lt => String::from("<"),
-        NumberBinaryOp::Eq => String::from("="),
-        NumberBinaryOp::Ge => String::from(">="),
-        NumberBinaryOp::Le => String::from("<="),
-        NumberBinaryOp::Ne => String::from("!="),
-        NumberBinaryOp::Shl => String::from(">>"),
-        NumberBinaryOp::Shr => String::from("<<"),
-        NumberBinaryOp::Bor => String::from("|"),
-        NumberBinaryOp::Band => String::from("&"),
-        NumberBinaryOp::Swap => String::from("swap"),
-        NumberBinaryOp::Over => String::from("over"),
-    };
+pub fn unaryop_readable(x: UnaryOp) -> String {
+    match x {
+        UnaryOp::Print => String::from("print"),
+        UnaryOp::Dup => String::from("dup"),
+        UnaryOp::Drop => String::from("drop"),
+    }
 }
 
 
@@ -100,61 +99,104 @@ impl<'a> Runtime<'a> {
         }
     }
 
-    fn unop_number(&mut self, span: TokenSpan, x: NumberUnaryOp) -> Result<(), RuntimeError> {
+    fn unop(&mut self, span: TokenSpan, x: UnaryOp) -> Result<(), RuntimeError> {
         if let Some(a) = self.pop() {
             match a {
                 Data::Number(n) => match x {
-                    NumberUnaryOp::Print => println!("{}", n),
-                    NumberUnaryOp::Dup => {
+                    UnaryOp::Print => println!("{}", n),
+                    UnaryOp::Dup => {
                         self.push_number(n);
                         self.push_number(n);
                     },
-                    // NOTE: this actually drops twice because of the pop at the if let above that already removed the value
-                    // NumberUnaryOp::Drop => self.drop(),
-                    NumberUnaryOp::Drop => {},
+                    UnaryOp::Drop => {},
                 },
-                Data::String(_) => return Err(RuntimeError::UnexpectedType(span, numberunaryop_readable(x), "(number)".to_string(), "(string)".to_string())),
+                Data::String(s) => match x {
+                    UnaryOp::Print => println!("{}", s),
+                    UnaryOp::Dup => {
+                        self.push_string(s.clone());
+                        self.push_string(s);
+                    },
+                    UnaryOp::Drop => {},
+                }
             }
         } else {
-            return Err(RuntimeError::StackUnderflow(span, numberunaryop_readable(x), 1));
+            return Err(RuntimeError::StackUnderflow(span, unaryop_readable(x), 1));
         }
         Ok(())
     }
 
-    fn binop_number(&mut self, span: TokenSpan, x: NumberBinaryOp) -> Result<(), RuntimeError> {
+    fn binop(&mut self, span: TokenSpan, x: BinaryOp) -> Result<(), RuntimeError> {
         if let (Some(a), Some(b)) = (self.pop(), self.pop()) {
             match (a, b) {
                 (Data::Number(n1), Data::Number(n2)) => match x {
-                    NumberBinaryOp::Add => self.push_number(n1 + n2),
-                    NumberBinaryOp::Sub => self.push_number(n1 - n2),
-                    NumberBinaryOp::Mul => self.push_number(n1 * n2),
-                    NumberBinaryOp::Div => self.push_number(n1 / n2),
-                    NumberBinaryOp::Eq => self.push_number((n1 == n2) as i32 as f64),
-                    NumberBinaryOp::Ne => self.push_number((n1 != n2) as i32 as f64),
-                    NumberBinaryOp::Lt => self.push_number((n1 < n2) as i32 as f64),
-                    NumberBinaryOp::Gt => self.push_number((n1 > n2) as i32 as f64),
-                    NumberBinaryOp::Le => self.push_number((n1 <= n2) as i32 as f64),
-                    NumberBinaryOp::Ge => self.push_number((n1 >= n2) as i32 as f64),
-                    NumberBinaryOp::Shl => self.push_number(((n1 as i32) << (n2 as i32)) as f64),
-                    NumberBinaryOp::Shr => self.push_number(((n1 as i32) >> (n2 as i32)) as f64),
-                    NumberBinaryOp::Bor => self.push_number(((n1 as i32) | (n2 as i32)) as f64),
-                    NumberBinaryOp::Band => self.push_number(((n1 as i32) & (n2 as i32)) as f64),
-                    NumberBinaryOp::Swap => {
+                    BinaryOp::Add => self.push_number(n1 + n2),
+                    BinaryOp::Sub => self.push_number(n1 - n2),
+                    BinaryOp::Mul => self.push_number(n1 * n2),
+                    BinaryOp::Div => self.push_number(n1 / n2),
+                    BinaryOp::Eq => self.push_number((n1 == n2) as i32 as f64),
+                    BinaryOp::Ne => self.push_number((n1 != n2) as i32 as f64),
+                    BinaryOp::Lt => self.push_number((n1 < n2) as i32 as f64),
+                    BinaryOp::Gt => self.push_number((n1 > n2) as i32 as f64),
+                    BinaryOp::Le => self.push_number((n1 <= n2) as i32 as f64),
+                    BinaryOp::Ge => self.push_number((n1 >= n2) as i32 as f64),
+                    BinaryOp::Shl => self.push_number(((n1 as i32) << (n2 as i32)) as f64),
+                    BinaryOp::Shr => self.push_number(((n1 as i32) >> (n2 as i32)) as f64),
+                    BinaryOp::Bor => self.push_number(((n1 as i32) | (n2 as i32)) as f64),
+                    BinaryOp::Band => self.push_number(((n1 as i32) & (n2 as i32)) as f64),
+                    BinaryOp::Swap => {
                         self.push_number(n1);
                         self.push_number(n2);
                     },
-                    NumberBinaryOp::Over => {
+                    BinaryOp::Over => {
                         self.push_number(n2);
                         self.push_number(n1);
                         self.push_number(n2);
                     },
                 },
-                (Data::String(_), Data::String(_)) => return Err(RuntimeError::UnexpectedType(span, numberbinaryop_readable(x), "(number, number)".to_string(), "(string, string)".to_string())),
-                (Data::String(_), Data::Number(_)) => return Err(RuntimeError::UnexpectedType(span, numberbinaryop_readable(x), "(number, number)".to_string(), "(string, number)".to_string())),
-                (Data::Number(_), Data::String(_)) => return Err(RuntimeError::UnexpectedType(span, numberbinaryop_readable(x), "(number, number)".to_string(), "(number, string)".to_string())),
+                (Data::String(s1), Data::String(s2)) => match x {
+                    BinaryOp::Add => self.push_string(s1 + &s2),
+                    BinaryOp::Eq => self.push_number((s1 == s2) as i32 as f64),
+                    BinaryOp::Ne => self.push_number((s1 != s2) as i32 as f64),
+                    // other comparison operators are not (should not be) supported for strings
+                    // BinaryOp::Lt => self.push_number((s1 < s2) as i32 as f64),
+                    // BinaryOp::Gt => self.push_number((s1 > s2) as i32 as f64),
+                    // BinaryOp::Le => self.push_number((s1 <= s2) as i32 as f64),
+                    // BinaryOp::Ge => self.push_number((s1 >= s2) as i32 as f64),
+                    BinaryOp::Swap => {
+                        self.push_string(s1);
+                        self.push_string(s2);
+                    },
+                    BinaryOp::Over => {
+                        self.push_string(s2.clone());
+                        self.push_string(s1.clone());
+                        self.push_string(s2);
+                    },
+                    _ => return Err(RuntimeError::UnexpectedType(
+                        span,
+                        binaryop_readable(x),
+                        "(number, number)".to_string(),
+                        "(string, string)".to_string(),
+                    )),
+                },
+                (Data::String(_), Data::Number(_)) => {
+                    return Err(RuntimeError::UnexpectedType(
+                        span,
+                        binaryop_readable(x),
+                        "(number, number) or (string, string)".to_string(),
+                        "(number, string)".to_string(),
+                    ));
+                },
+                (Data::Number(_), Data::String(_)) => {
+                    return Err(RuntimeError::UnexpectedType(
+                        span,
+                        binaryop_readable(x),
+                        "(number, number) or (string, string)".to_string(),
+                        "(string, number)".to_string(),
+                    ));
+                },
             }
         } else {
-            return Err(RuntimeError::StackUnderflow(span, numberbinaryop_readable(x), 2));
+            return Err(RuntimeError::StackUnderflow(span, binaryop_readable(x), 2));
         }
         Ok(())
     }
@@ -189,25 +231,25 @@ impl<'a> Runtime<'a> {
             Node::Operation(op, s) => {
                 let s = s.clone(); // TODO: this is a hack, fix it
                 match op {
-                    OpKind::Add => self.binop_number(s, NumberBinaryOp::Add)?,
-                    OpKind::Sub => self.binop_number(s, NumberBinaryOp::Sub)?,
-                    OpKind::Mul => self.binop_number(s, NumberBinaryOp::Mul)?,
-                    OpKind::Div => self.binop_number(s, NumberBinaryOp::Div)?,
-                    OpKind::Gt => self.binop_number(s, NumberBinaryOp::Gt)?,
-                    OpKind::Lt => self.binop_number(s, NumberBinaryOp::Lt)?,
-                    OpKind::Eq => self.binop_number(s, NumberBinaryOp::Eq)?,
-                    OpKind::Ge => self.binop_number(s, NumberBinaryOp::Ge)?,
-                    OpKind::Le => self.binop_number(s, NumberBinaryOp::Le)?,
-                    OpKind::Ne => self.binop_number(s, NumberBinaryOp::Ne)?,
-                    OpKind::Shl => self.binop_number(s, NumberBinaryOp::Shl)?,
-                    OpKind::Shr => self.binop_number(s, NumberBinaryOp::Shr)?,
-                    OpKind::Bor => self.binop_number(s, NumberBinaryOp::Bor)?,
-                    OpKind::Band => self.binop_number(s, NumberBinaryOp::Band)?,
-                    OpKind::Swap => self.binop_number(s, NumberBinaryOp::Swap)?,
-                    OpKind::Over => self.binop_number(s, NumberBinaryOp::Over)?,
-                    OpKind::Dup => self.unop_number(s, NumberUnaryOp::Dup)?,
-                    OpKind::Drop => self.unop_number(s, NumberUnaryOp::Drop)?,
-                    OpKind::Print => self.unop_number(s, NumberUnaryOp::Print)?,
+                    OpKind::Add => self.binop(s, BinaryOp::Add)?,
+                    OpKind::Sub => self.binop(s, BinaryOp::Sub)?,
+                    OpKind::Mul => self.binop(s, BinaryOp::Mul)?,
+                    OpKind::Div => self.binop(s, BinaryOp::Div)?,
+                    OpKind::Gt => self.binop(s, BinaryOp::Gt)?,
+                    OpKind::Lt => self.binop(s, BinaryOp::Lt)?,
+                    OpKind::Eq => self.binop(s, BinaryOp::Eq)?,
+                    OpKind::Ge => self.binop(s, BinaryOp::Ge)?,
+                    OpKind::Le => self.binop(s, BinaryOp::Le)?,
+                    OpKind::Ne => self.binop(s, BinaryOp::Ne)?,
+                    OpKind::Shl => self.binop(s, BinaryOp::Shl)?,
+                    OpKind::Shr => self.binop(s, BinaryOp::Shr)?,
+                    OpKind::Bor => self.binop(s, BinaryOp::Bor)?,
+                    OpKind::Band => self.binop(s, BinaryOp::Band)?,
+                    OpKind::Swap => self.binop(s, BinaryOp::Swap)?,
+                    OpKind::Over => self.binop(s, BinaryOp::Over)?,
+                    OpKind::Dup => self.unop(s, UnaryOp::Dup)?,
+                    OpKind::Drop => self.unop(s, UnaryOp::Drop)?,
+                    OpKind::Print => self.unop(s, UnaryOp::Print)?,
                     OpKind::Rot => { todo!() },
                 }
             },
